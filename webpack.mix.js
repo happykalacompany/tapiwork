f5dc54784ca2fe7eeabf638fb6b3d779168fe8eb
happykala <1030775250@qq.com>
2018-11-01 18:09:58 +0800

    1、使用scss 2、配置webpack.mix.js声明别名以便在组件中方便的引入scss资源，并使得组件中的资源文件可以被编译进来 3、编写导航栏组件

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,25 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
+    .webpackConfig({
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules(?!\/foundation-sites)|bower_components/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: Config.babel()
+                        }
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve('resources/assets/sass')
+            }
+        }
+    })
    .sass('resources/assets/sass/app.scss', 'public/css');
