882614e2af99ebf5f663112384fa207bb17f131d
happykala <1030775250@qq.com>
2018-11-02 11:28:46 +0800

    删除导航组件

---
+++ resources/assets/js/pages/Layout.vue
@@ -0,0 +1,16 @@
+<style>
+
+</style>
+<template>
+    <div id='app-layout'>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+// import Navgation from '../components/global/Navgation.vue';
+
+export default {
+    
+}
+</script>
+
--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -20,26 +20,33 @@
  * 定义路由
  */
  export default new VueRouter({
-     routes:[
-         {
+    routes:[
+        {
             path:'/',
-            name:'home',
-            component:Vue.component('Home',require('./pages/Home.vue'))
-         },
-         {
-           path:'/cafes',
-           name:'cafes',
-           component:Vue.component('Cafes',require('./pages/Cafes.vue')) 
-         },
-         {
-             path:'/cafes/new',
-             name:'newcafe',
-             component:Vue.component('NewCafe',require('./pages/NewCafe.vue'))
-         },
-         {
-             path:'/cafes/:id',
-             name:'cafe',
-             component:Vue.component('Cafe',require('./pages/Cafe.vue'))
-         }
-     ]
+            name:'layout',
+            component:Vue.component('Home',require('./pages/Layout.vue')),
+            children:[
+                    {
+                        path:'home',
+                        name:'home',
+                        component:Vue.component('Home',require('./pages/Home.vue'))
+                     },
+                     {
+                       path:'cafes',
+                       name:'cafes',
+                       component:Vue.component('Cafes',require('./pages/Cafes.vue')) 
+                     },
+                     {
+                         path:'cafes/new',
+                         name:'newcafe',
+                         component:Vue.component('NewCafe',require('./pages/NewCafe.vue'))
+                     },
+                     {
+                         path:'cafes/:id',
+                         name:'cafe',
+                         component:Vue.component('Cafe',require('./pages/Cafe.vue'))
+                     }
+            ]
+        }
+    ]
  });
